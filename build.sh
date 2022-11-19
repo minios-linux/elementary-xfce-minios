d5a3dcda5b6ddc52c8b64b2a60674e7eb8a58ac8
crims0n <crims0n@ya.ru>
2022-11-20 01:46:40 +0300

    Update

--- build.sh
+++ build.sh
@@ -42,7 +42,7 @@ for FOLDER in $(ls $SOURCE | grep -E "^[0-9]+$"); do
 done
 
 cd $SCRIPT_DIR
-tar --exclude-vcs -zcf elementary-xfce-minios_$(dpkg-parsechangelog --show-field Version | sed "s/-1//g").orig.tar.gz ./elementary-xfce
+tar --exclude-vcs -zcf elementary-xfce-minios_$(dpkg-parsechangelog --show-field Version | sed "s/-2-2/-2/g").orig.tar.gz ./elementary-xfce
 
 cd $SCRIPT_DIR/elementary-xfce
 apt build-dep elementary-xfce
