d3561b6c17ff8070c7891d1fe1b0d25c9c3d0efb
crim50n <crims0n@yandex.ru>
2023-09-13 23:15:23 +0300

    Update 20230913

--- build.sh
+++ build.sh
@@ -7,38 +7,36 @@ if apt-cache policy git | grep -q "Installed: (none)"; then
     apt-get install -y git
 fi
 
-git submodule init
-git submodule update
+git submodule init ./
+git submodule update ./
+
+lnsvg() {
+    local FILE LINK
+    FILE=$1
+    LINK=$2
+    if [ -f $FILE ]; then
+        ln -s $FILE $LINK
+    fi
+}
 
 cp -R $SCRIPT_DIR/debian $SCRIPT_DIR/elementary-xfce/
 
 for FOLDER in $(ls $SOURCE | grep -E "^[0-9]+$"); do
     cd $SOURCE/$FOLDER
-    if [ -f accessories-calculator.svg ]; then
-        ln -s accessories-calculator.svg galculator.svg
-    fi
     if [ -f baobab.svg ]; then
-        ln -s baobab.svg org.gnome.baobab.svg
+        lnsvg baobab.svg org.gnome.baobab.svg
     elif [ -f org.gnome.baobab.svg ]; then
-        ln -s org.gnome.baobab.svg baobab.svg
+        lnsvg org.gnome.baobab.svg baobab.svg
     fi
-    if [ -f internet-mail.svg ]; then
-        ln -s internet-mail.svg emblem-mail.svg
-    fi
-    if [ -f preferences-system-power.svg ]; then
-        ln -s preferences-system-power.svg laptop-mode-tools.svg
-    fi
-    if [ -f pdfshuffler.svg ]; then
-        ln -s pdfshuffler.svg com.github.jeromerobert.pdfarranger.svg
-        ln -s pdfshuffler.svg pdfarranger.svg
-        ln -s pdfshuffler.svg pdfmod.svg
-    fi
-    if [ -f ../../status/$FOLDER/sync-synchronizing.svg ]; then
-        ln -s ../../status/$FOLDER/sync-synchronizing.svg grsync.svg
-    fi
-    if [ -f ../../devices/$FOLDER/drive-harddisk.svg ]; then
-        ln -s ../../devices/$FOLDER/drive-harddisk.svg gsmartcontrol.svg
-    fi
+    lnsvg accessories-calculator.svg galculator.svg
+    lnsvg internet-mail.svg emblem-mail.svg
+    lnsvg preferences-system-power.svg laptop-mode-tools.svg
+    lnsvg pdfshuffler.svg com.github.jeromerobert.pdfarranger.svg
+    lnsvg pdfshuffler.svg pdfarranger.svg
+    lnsvg pdfshuffler.svg pdfmod.svg
+    lnsvg ../../status/$FOLDER/sync-synchronizing.svg grsync.svg
+    lnsvg ../../devices/$FOLDER/drive-harddisk.svg gsmartcontrol.svg
+    lnsvg utilities-system-monitor.svg qps.svg
 done
 
 cd $SCRIPT_DIR
