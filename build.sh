a249534fd6f3c71e9dd59959ce9596c75477dd84
crim50n <crims0n@yandex.ru>
2023-09-14 00:32:55 +0300

    Update 20230914

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-SOURCE=$SCRIPT_DIR/elementary-xfce/elementary-xfce/apps
+ELEMENTARY_APPS=$SCRIPT_DIR/elementary-xfce/elementary-xfce/apps
+ELEMENTARY_ACTIONS=$SCRIPT_DIR/elementary-xfce/elementary-xfce/actions
+MINIOS_APPS=$SCRIPT_DIR/minios-icons/apps
 
 if apt-cache policy git | grep -q "Installed: (none)"; then
     apt-get install -y git
@@ -21,8 +23,13 @@ lnsvg() {
 
 cp -R $SCRIPT_DIR/debian $SCRIPT_DIR/elementary-xfce/
 
-for FOLDER in $(ls $SOURCE | grep -E "^[0-9]+$"); do
-    cd $SOURCE/$FOLDER
+for FOLDER in $(ls $MINIOS_APPS | grep -E "^[0-9]+$"); do
+    cd $MINIOS_APPS/$FOLDER
+    cp *.svg $ELEMENTARY_APPS/$FOLDER
+done
+
+for FOLDER in $(ls $ELEMENTARY_APPS | grep -E "^[0-9]+$"); do
+    cd $ELEMENTARY_APPS/$FOLDER
     if [ -f baobab.svg ]; then
         lnsvg baobab.svg org.gnome.baobab.svg
     elif [ -f org.gnome.baobab.svg ]; then
@@ -37,10 +44,20 @@ for FOLDER in $(ls $SOURCE | grep -E "^[0-9]+$"); do
     lnsvg ../../status/$FOLDER/sync-synchronizing.svg grsync.svg
     lnsvg ../../devices/$FOLDER/drive-harddisk.svg gsmartcontrol.svg
     lnsvg utilities-system-monitor.svg qps.svg
+    lnsvg web-browser.svg falkon.svg
+    lnsvg clipboard.svg qlipper.svg
+    lnsvg clipboard.svg xfce4-clipman-plugin.png
+done
+
+for FOLDER in $(ls $ELEMENTARY_ACTIONS | grep -E "^[0-9]+$"); do
+    lnsvg view-grid-symbolic.svg view-list.svg
+    lnsvg view-dual-symbolic.svg view-preview.svg
+    lnsvg view-compact-symbolic.svg view-list-text.svg
+    lnsvg view-list-symbolic.svg view-list-detalis.svg
 done
 
 cd $SCRIPT_DIR
-tar --exclude-vcs -zcf elementary-xfce-minios_$(dpkg-parsechangelog --show-field Version | sed "s/-2~mos+1//g").orig.tar.gz ./elementary-xfce
+tar --exclude-vcs -zcf elementary-xfce-minios_$(dpkg-parsechangelog --show-field Version | sed "s/-1~mos+1//g").orig.tar.gz ./elementary-xfce
 
 cd $SCRIPT_DIR/elementary-xfce
 apt build-dep elementary-xfce
